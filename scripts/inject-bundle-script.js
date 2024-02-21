24bc93e7894768364aae723921df9b59376bf8a5
richardsnider <richardsnider@protonmail.com>
2024-02-21 13:29:50 -0800

    minify bundle

--- scripts/inject-bundle-script.js
+++ scripts/inject-bundle-script.js
@@ -5,8 +5,12 @@ const bundlePath = path.resolve(__dirname, '../dist/bundle.js');
 const indexPath = path.resolve(__dirname, '../dist/index.html');
 const bundle = fs.readFileSync(bundlePath, 'utf8');
 const index = fs.readFileSync(indexPath, 'utf8');
-const newScriptTag = `<script type="module" async>${bundle}</script>`; 
-const newHTML = index.replace('</body>', newScriptTag + '</body>');
+const newHTML = index.replace(`</body>`,
+`<script>
+${bundle}
+</script>
+</body>`);
+
 fs.writeFileSync(indexPath, newHTML);
 
 console.log('Injected bundle.js into index.html');
